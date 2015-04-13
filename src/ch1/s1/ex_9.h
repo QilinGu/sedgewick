42298e03b04b3c1dd827f6f5f8a3d6a56e7d2635
Samvel Khalatyan <sn.khalatyan@gmail.com>
2015-04-13 13:20:43 -0700

    comply guards with google style

--- src/ch1/s1/ex_9.h
+++ src/ch1/s1/ex_9.h
@@ -1,5 +1,5 @@
-#ifndef CH1_S1_EX9_H_
-#define CH1_S1_EX9_H_
+#ifndef SEDGEWICK_CH1_S1_EX9_H_
+#define SEDGEWICK_CH1_S1_EX9_H_
 
 #include <cstddef>
 #include <string>
@@ -7,10 +7,12 @@
 namespace ch1 {
 namespace s1 {
 namespace ex9 {
-    // return binary representation of unsigned vlaue:
-    //      size_t value {15};
-    //      std::string bin_representation {BinaryString(value)};
-    std::string BinaryString(size_t);
+
+// return binary representation of unsigned vlaue:
+//      size_t value {15};
+//      std::string bin_representation {BinaryString(value)};
+std::string BinaryString(size_t);
+
 }  // namespace ex9
 }  // namespace s1
 }  // namespace ch1
