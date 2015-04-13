b719de8c42cbaaec86a6f39a176094f84adaae07
Samvel Khalatyan <sn.khalatyan@gmail.com>
2015-04-13 10:11:16 -0700

    p1.1.9 to binary string

---
+++ ch1/fwd.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+
+// p1.1.9
+std::string BinaryString(size_t);
---
+++ ch1/ut/1_1_9.cpp
@@ -0,0 +1,40 @@
+#include "ch1/fwd.h"
+
+#include "gtest/gtest.h"
+
+#include <cstddef>
+#include <initializer_list>
+#include <string>
+#include <utility>
+
+TEST(BinaryString, Numbers) {
+    using TItem = std::pair<size_t, std::string>;
+    using TData = std::initializer_list<TItem>;
+    TData data {
+          {0, "0"}
+        , {1, "1"}
+        , {2, "10"}
+        , {3, "11"}
+        , {4, "100"}
+        , {5, "101"}
+        , {6, "110"}
+        , {7, "111"}
+        , {8, "1000"}
+        , {9, "1001"}
+        , {10, "1010"}
+        , {11, "1011"}
+        , {12, "1100"}
+        , {13, "1101"}
+        , {14, "1110"}
+        , {15, "1111"}
+    };
+    for(auto &d : data) {
+        std::string bin_str {BinaryString(d.first)};
+        EXPECT_STREQ(bin_str.c_str(), d.second.c_str()) << d.first;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
